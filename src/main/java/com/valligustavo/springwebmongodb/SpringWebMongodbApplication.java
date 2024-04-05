d2f569253a6fff0e98bc8c3dcf4657b6b0185d3a
GustavoValli <valligustavo2005@gmail.com>
2024-04-05 14:08:13 -0300

    Create Project

---
+++ src/main/java/com/valligustavo/springwebmongodb/SpringWebMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.valligustavo.springwebmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valligustavo/springwebmongodb/SpringWebMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valligustavo.springwebmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
