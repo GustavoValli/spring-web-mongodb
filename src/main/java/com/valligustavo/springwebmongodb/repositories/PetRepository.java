ad814bd9b38cf53455b20ca1b397d84f3bf250bf
GustavoValli <valligustavo2005@gmail.com>
2024-05-06 16:16:51 -0300

    Import fix

--- src/main/java/com/valligustavo/springwebmongodb/repositories/PetRepository.java
+++ src/main/java/com/valligustavo/springwebmongodb/repositories/PetRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface PetRepository extends MongoRepository<Pet, String> {
     @Query("{ 'name': { $regex: ?0, $options: 'i' }, 'size': { $regex: ?1, $options: 'i' }, 'age': ?2 }")
